167619799c2e0716f140eb8e4fcf00229917509c
lzamzamm <notlucy78@gmail.com>
2024-07-24 15:23:14 +0700

    test1

--- fizzbuzz-test.js
+++ fizzbuzz-test.js
@@ -1,13 +1,11 @@
-for (var i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
-    n1 = i % 3;
-    n2 = i % 5;
     console.log(`${i} = FizzBuzz`);
   } else if (i % 5 == 0) {
-    n = i % 5;
     console.log(`${i} = Buzz`);
   } else if (i % 3 == 0) {
-    n = i % 3;
     console.log(`${i} = Fizz`);
+  } else {
+    console.log(i);
   }
 }
