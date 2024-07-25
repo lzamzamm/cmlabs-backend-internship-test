4a2a67d130e4b2795af15699459ba2353ed1d64e
lzamzamm <notlucy78@gmail.com>
2024-07-25 13:10:04 +0700

    test 1

--- fizzbuzz-test.js
+++ fizzbuzz-test.js
@@ -1,9 +1,12 @@
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
+  const isDivisibleBy3 = i % 3 === 0;
+  const isDivisibleBy5 = i % 5 === 0;
+
+  if (isDivisibleBy3 && isDivisibleBy5) {
     console.log(`${i} = FizzBuzz`);
-  } else if (i % 5 == 0) {
+  } else if (isDivisibleBy5) {
     console.log(`${i} = Buzz`);
-  } else if (i % 3 == 0) {
+  } else if (isDivisibleBy3) {
     console.log(`${i} = Fizz`);
   } else {
     console.log(i);
